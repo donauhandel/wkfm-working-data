0a7c829ce71cf983f429caeb0daafe83786df842
csae8092 <peter.andorfer@oeaw.ac.at>
2023-07-25 10:19:43 +0200

    better data

---
+++ check_segs.py
@@ -0,0 +1,19 @@
+import glob
+from acdh_tei_pyutils.tei import TeiReader
+
+
+files = glob.glob("./data/editions/*.xml")
+
+counter = 0 
+for x in files:
+    doc = TeiReader(x)
+    segs = doc.any_xpath(".//tei:seg[starts-with(@type, 'orighead')]")
+    seg_count = len(segs)
+    if seg_count == 3:
+        pass
+    elif seg_count == 5:
+        pass
+    else:
+        print(x, seg_count)
+        counter += 1
+print(counter)
